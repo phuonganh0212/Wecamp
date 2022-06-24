08e0ec6245fed6ea8b370356f57dddde51c1dbf6
phuonganh0212 <108012310+phuonganh0212@users.noreply.github.com>
2022-06-24 09:13:49 +0700

    Add files via upload

---
+++ New1.java
@@ -0,0 +1,10 @@
+public class New1 {
+    public void showSum(int a, int b, int c) {
+        int sum = a + b + c;
+        System.out.println(sum);
+    }
+    public static void main(String[] args) {
+    New1 app = new New1 ();
+    app.showSum(4,6,7); 
+    }
+}
